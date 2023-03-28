bfe102bb5ca57a06d9bb4d494e1a9500f77ab552
Mikail KOCADAĞ <91754647+MKLKCDG@users.noreply.github.com>
2023-03-28 18:18:57 +0300

    page

---
+++ index.php
@@ -0,0 +1,16 @@
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Loding...</title>
+    <meta http-equiv="refresh" content="10;URL=./index/">
+        <link rel="stylesheet" href="./UI/assets/css/preloader/preloader.css">
+  </head>
+
+  <body>
+    <div class="load">
+  <pre class="term">guest@mkocadag.net </pre>
+</div>
+    <script src='./index/assets/js/jquery-3.6.0.min.js'></script>
+        <script src="./index/assets/js/preloader/preloader.js"></script>
+  </body>
+</html>
---
+++ index/index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Mikail KOCADAĞ</title>
+<link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon" >
+
+
+
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+<link rel="stylesheet" href="../css/font-awesome.min.css">
+
+<!-- Main css -->
+<link rel="stylesheet" href="../css/style.css">
+<link href="https://fonts.googleapis.com/css?family=Lora|Merriweather:300,400" rel="stylesheet">
+
+</head>
+<body>
+
+<!-- PRE LOADER -->
+
+<div class="preloader">
+     <div class="sk-spinner sk-spinner-wordpress">
+          <span class="sk-inner-circle"></span>
+     </div>
+</div>
+
+<!-- Navigation section  -->
+
+
+</div>
+
+<!-- Home Section -->
+
+<section id="home" class="main-home parallax-section">
+     <div class="overlay"></div>
+     <div id="particles-js"></div>
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-12 col-sm-12">
+                    <h1>Mikail Kocadağ</h1>
+                    <h4>Blog siteme hoşgeldin! şuan için tadilatdayız</h4>
+                    <!--<a href="#blog" class="smoothScroll btn btn-default">Tamam</a>-->
+               </div>
+
+          </div>
+     </div>
+</section>
+
+<!-- Blog Section -->
+
+
+
+<!-- Footer Section -->
+
+<footer>
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-5 col-md-offset-1 col-sm-6">
+                    <h3>Mikail</h3>
+                    <p>Her zaman her yerde kendini geliştiren birisi.</p>
+                    
+                    <div class="footer-copyright">
+                         <p>Copyright &copy; 2023</p>
+                    </div>
+               </div>
+
+               <div class="col-md-4 col-md-offset-1 col-sm-6">
+                    <h3>Contact Me</h3>
+                    
+                    <p><i class="fa fa-save"></i>mikailkocadag68@gmail.com</p>
+               </div>
+             
+
+               <div class="clearfix col-md-12 col-sm-12">
+                    <hr>
+               </div>
+              
+               <div class="col-md-12 col-sm-12">
+               <div class="container d-flex justify-content-center ">
+                       <img class="altlogo" src="https://www.sibervatan.org/assets/img/sibervatanlogo.png" alt="">
+                    </div>
+                    <ul class="social-icon">
+                         <li><a href="https://tryhackme.com/p/GrizzlY" class="fa fa-tryhackme"></a></li>
+                         <li><a href="https://github.com/MKLKCDG" class="fa fa-github"></a></li>
+                         <li><a href="https://www.linkedin.com/in/mikailkocadag/" class="fa fa-linkedin"></a></li>
+                    </ul>
+                   
+               </div>
+               
+          </div>
+     </div>
+</footer>
+
+<!-- Back top -->
+<a href="#back-top" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+<!-- SCRIPTS -->
+
+<script src="../js/jquery.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/particles.min.js"></script>
+<script src="../js/app.js"></script>
+<script src="../js/jquery.parallax.js"></script>
+<script src="../js/smoothscroll.js"></script>
+<script src="../js/custom.js"></script>
+
+</body>
+</html>
